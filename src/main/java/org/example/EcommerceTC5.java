4b4c82fb705ace3d5fdfe9d1c63c1500e5bcc206
AntonStrakhov <gordnet_ctpax@mail.ru>
2024-07-10 09:06:40 +0300

    added test how to switch from native to web

---
+++ src/main/java/org/example/EcommerceTC5.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.ElementOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.net.MalformedURLException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
+
+public class EcommerceTC5 extends Base {
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        AndroidDriver<AndroidElement> driver = capabilities("emulator");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@text='Female']")).click();
+        driver.findElement(By.id("android:id/text1")).click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+        // if the previous is not work use below
+        // driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"" + containedText + "\").instance(0))"));
+        driver.findElement(By.xpath("//*[@text='Argentina']")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+        driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
+        driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+
+        //Mobile gestures
+
+        WebElement checkBox = driver.findElement(By.className("android.widget.CheckBox"));
+        TouchAction t = new TouchAction(driver);
+        t.tap(tapOptions().withElement(ElementOption.element(checkBox))).perform();
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+        Thread.sleep(7000);
+        Set<String> contexts = driver.getContextHandles();
+        for (String contextName : contexts) {
+            System.out.println(contextName);
+        }
+        driver.context("WEBVIEW_com.androidsample.generalstore");
+        driver.findElement(By.name("q")).sendKeys("Hello");
+    }
+}
