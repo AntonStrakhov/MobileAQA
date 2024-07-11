a210c91b9a65904e7c681adbbca48624ad60963e
AntonStrakhov <gordnet_ctpax@mail.ru>
2024-07-11 08:16:06 +0300

    desired capabilities to automate mobile browser

---
+++ src/main/java/org/example/Browse.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Browse {
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        AndroidDriver<AndroidElement> driver;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "ANSTR50");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
