f2befa838ef8e4275dc356da6f3ff4990c0aeb15
AntonStrakhov <gordnet_ctpax@mail.ru>
2024-07-17 09:15:21 +0300

    added iOS tap test

--- src/main/java/org/example/LongPressTapIOSTest.java
+++ src/main/java/org/example/LongPressTapIOSTest.java
@@ -3,11 +3,13 @@ package org.example;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSTouchAction;
+import io.appium.java_client.touch.TapOptions;
 
 import java.net.MalformedURLException;
 import java.time.Duration;
 
 import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
 import static java.time.Duration.ofSeconds;
 
@@ -19,5 +21,8 @@ public class LongPressTapIOSTest extends BaseIOSTest{
         MobileElement e = (MobileElement)driver.findElementByName("Long tap");
         IOSTouchAction touch = new IOSTouchAction(driver);
         touch.longPress(longPressOptions().withElement(element(e)).withDuration(ofSeconds(2))).release().perform();
+
+        MobileElement tap = (MobileElement)driver.findElementByXPath("//XCUIElementTypeSwitch[1]");
+        touch.tap(tapOptions().withElement(element(tap))).perform();
     }
 }
