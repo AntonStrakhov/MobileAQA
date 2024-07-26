bc5338b90df7f2c62f03965a9679c5d68e1afeb3
AntonStrakhov <gordnet_ctpax@mail.ru>
2024-07-26 09:02:56 +0300

    added page object and factory patterns

--- src/main/java/org/example/Basics.java
+++ src/main/java/org/example/Basics.java
@@ -2,6 +2,8 @@ package org.example;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import pageObject.HomePage;
+import pageObject.Preferences;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
@@ -13,11 +15,19 @@ public class Basics extends Base {
         AndroidDriver<AndroidElement> driver = capabilities("real");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
-        driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+        HomePage h = new HomePage(driver);
+
+        //driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        h.Preferences.click();
+
+        Preferences p = new Preferences(driver);
+
+        //driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+        p.dependencies.click();
         driver.findElementById("android:id/checkbox").click();
         driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
         driver.findElementByClassName("android.widget.EditText").sendKeys("hello");
-        driver.findElementsByClassName("android.widget.Button").get(1).click();
+        //driver.findElementsByClassName("android.widget.Button").get(1).click();
+        p.buttons.get(1).click();
     }
 }
